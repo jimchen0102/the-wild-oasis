a4017f4888f6f759add2b1f98d46a6f1ea0bfee5
jim.chen <azx011014@gmail.com>
2023-10-23 22:31:40 +0800

    [feature]: add filtering cabins

--- src/ui/Modal.jsx
+++ src/ui/Modal.jsx
@@ -57,7 +57,6 @@ const ModalContext = createContext();
 
 function Modal({ children }) {
   const [openName, setOpenName] = useState("");
-
   const close = () => setOpenName("");
   const open = setOpenName;
 
