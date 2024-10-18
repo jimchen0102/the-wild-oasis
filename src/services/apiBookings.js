d1c778614f3fc4f2d20e6ff6f5de751a7ee190dd
jim.chen <azx011014@gmail.com>
2024-10-18 11:27:01 +0800

    feat: implement default and custom booking sorting
    - Add sorting feature for bookings based on `sortBy` parameter in `useBookings()` hook and `getBookings()` function.
    - Integrate default sorting by `startDate` in ascending order if no sort parameter is provided in `useBookings()`.
    
    Signed-off-by: jim.chen <azx011014@gmail.com>

--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -12,6 +12,12 @@ export async function getBookings({ filter, sortBy }) {
     query = query.eq(filter.field, filter.value);
   }
 
+  if (sortBy) {
+    query = query.order(sortBy.field, {
+      ascending: sortBy.direction === "asc",
+    });
+  }
+
   const { data, error } = await query;
 
   if (error) {
