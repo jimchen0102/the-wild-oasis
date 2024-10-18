450d5e0bc8634466451c6e217a8311165309a26a
jim.chen <azx011014@gmail.com>
2024-10-18 21:50:51 +0800

    feat: improve signup process with custom hooks and logic
    - Integrate `useSignup` custom hook in `SignupForm` for handling user registration logic
    - Disable form fields and buttons while signup is in progress
    - Modify `onSubmit` in `SignupForm` to utilize the `signup` function and reset form post submission
    - Add a new custom React hook `useSignup` leveraging `react-query` for user signup
    - Add a new `signup` function in `apiAuth.js` to handle user signups with Supabase and manage errors
    
    Signed-off-by: jim.chen <azx011014@gmail.com>

---
+++ src/features/authentication/useSignup.js
@@ -0,0 +1,20 @@
+import { useMutation } from "@tanstack/react-query";
+import { signup as signupApi } from "../../services/apiAuth";
+import { toast } from "react-hot-toast";
+
+export function useSignup() {
+  const { isLoading, mutate: signup } = useMutation({
+    mutationFn: signupApi,
+    onSuccess: (user) => {
+      console.log(user);
+      toast.success(
+        "Account successfully created! Please verify the new account from the email address."
+      );
+    },
+  });
+
+  return {
+    isLoading,
+    signup,
+  };
+}
