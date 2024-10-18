ae873e1bc69939b2ef572bd6aa02568f010be50b
jim.chen <azx011014@gmail.com>
2024-10-18 12:03:50 +0800

    feat: enhance booking data handling and prefetching
    - Add importing and usage of `useQueryClient` from `@tanstack/react-query`
    - Implement filtering, sorting, pagination, and pre-fetching logic in `useBookings` hook
    - Introduce constants import for `PAGE_SIZE` from `../../utils/constants`
    - Facilitate pre-fetching of next and previous pages in booking queries
    
    Signed-off-by: jim.chen <azx011014@gmail.com>

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,22 +1,28 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { getBookings } from "../../services/apiBookings";
 import { useSearchParams } from "react-router-dom";
+import { PAGE_SIZE } from "../../utils/constants";
 
 export function useBookings() {
+  const queryClient = useQueryClient();
   const [searchParams] = useSearchParams();
 
+  // FILTER
   const filterValue = searchParams.get("status") || "all";
   const filter =
     !filterValue || filterValue === "all"
       ? null
       : { field: "status", value: filterValue };
 
+  // SORT
   const sortByRaw = searchParams.get("sortBy") || "startDate-asc";
   const [field, direction] = sortByRaw.split("-");
   const sortBy = { field, direction };
 
+  // PAGINATION
   const page = searchParams.get("page") ? Number(searchParams.get("page")) : 1;
 
+  // QUERY
   const {
     isLoading,
     data: { data: bookings, count } = {},
@@ -25,6 +31,22 @@ export function useBookings() {
     queryKey: ["bookings", filter, sortBy, page],
     queryFn: () => getBookings({ filter, sortBy, page }),
   });
+
+  // PRE-FETCHING
+  const pageCount = Math.ceil(count / PAGE_SIZE);
+  if (page < pageCount) {
+    queryClient.prefetchQuery({
+      queryKey: ["bookings", filter, sortBy, page + 1],
+      queryFn: () => getBookings({ filter, sortBy, page: page + 1 }),
+    });
+  }
+
+  if (page > 1) {
+    queryClient.prefetchQuery({
+      queryKey: ["bookings", filter, sortBy, page - 1],
+      queryFn: () => getBookings({ filter, sortBy, page: page - 1 }),
+    });
+  }
 
   return {
     isLoading,
