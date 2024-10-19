51f5c8dc3ce2c6ab508208f58459ce4885174e19
jim.chen <azx011014@gmail.com>
2024-10-19 10:06:26 +0800

    feat: enhance UX for dynamic bookings and stays fetch
    - Add loading indicators and conditionally render a spinner in the DashboardLayout when fetching bookings or stays
    - Add new hooks for fetching recent bookings and stays with dynamic date ranges based on search parameters
    - Implement API calls for recent bookings and stays within newly added hooks
    
    Signed-off-by: jim.chen <azx011014@gmail.com>

---
+++ src/features/dashboard/useRecentStays.js
@@ -0,0 +1,28 @@
+import { useQuery } from "@tanstack/react-query";
+import { subDays } from "date-fns";
+import { useSearchParams } from "react-router-dom";
+import { getStaysAfterDate } from "../../services/apiBookings";
+
+export function useRecentStays() {
+  const [searchParams] = useSearchParams();
+
+  const numDays = searchParams.get("last")
+    ? Number(searchParams.get("last"))
+    : 7;
+  const queryDate = subDays(new Date(), numDays).toISOString();
+
+  const { isLoading, data: stays } = useQuery({
+    queryFn: () => getStaysAfterDate(queryDate),
+    queryKey: ["stays", `last-${numDays}`],
+  });
+
+  const confirmedStays = stays?.filter((stay) =>
+    ["checked-in", "checked-out"].includes(stay.status)
+  );
+
+  return {
+    isLoading,
+    stays,
+    confirmedStays,
+  };
+}
